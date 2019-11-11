408a3c9b281a55b6b6756feb42f8bbfac0ea6406
ag0stinh0 <t.sanches.araujo@gmail.com>
2019-11-11 20:31:52 -0300

    Working server

--- server.py
+++ server.py
@@ -2,12 +2,32 @@ import socket
 import sys
 import threading
 import base64
-import cv2
-import os
 import pytesseract as ocr
 import numpy as np
+import cv2
+import struct
 from PIL import Image
-from gtts import gTTS
+
+hostname = "redesb.space"
+HOST = socket.gethostbyname(hostName)	# Symbolic name meaning all available interfaces
+PORT = 9666	# Arbitrary non-privileged port
+HOST = socket.gethostbyname(HOST)
+print(str(HOST))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print ('Socket created')
+
+#Bind socket to local host and port
+try:
+	s.bind((HOST, PORT))
+except socket.error as msg:
+	print ('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+	sys.exit()
+
+print ('Socket bind complete')
+
+# Start listening on socket
+s.listen(10)
+print ('Socket now listening')
 
 
 # Receives base64 string and decode to jpg
@@ -27,17 +47,46 @@ def readImage():
 	cimg = np.array(imagem)
 	cimg = cimg[:, :, ::-1].copy()
 
+	# # Visualizacaoo da imagem
+	# cv2.imshow('image',cimg)
+	# cv2.waitKey(0)
+	# cv2.destroyAllWindows()
+
 	# convertendo em um array editavel de numpy[x, y, CANALS]
 	npimagem = np.asarray(imagem).astype(np.uint8)
+
+	# # Visualizacaoo da imagem
+	# cv2.imshow('image',npimagem)
+	# cv2.waitKey(0)
+	# cv2.destroyAllWindows()
 
 	# diminuio dos ruidos antes da binarizao
 	npimagem[:, :, 0] = 0 # zerando o canal R (RED)
 	npimagem[:, :, 2] = 0 # zerando o canal B (BLUE)
 
+	# # Visualizao da imagem
+	# cv2.imshow('image',npimagem)
+	# cv2.waitKey(0)
+	# cv2.destroyAllWindows()
+
 	# atribuio em escala de cinza
 	im = cv2.cvtColor(npimagem, cv2.COLOR_RGB2GRAY)
 
+	# # Visualizaaao da imagem
+	# cv2.imshow('image',im)
+	# cv2.waitKey(0)
+	# cv2.destroyAllWindows()
+
+	# apla da truncagem binaria para a intensidade
+	# pixels de intensidade de cor abaixo de 127 aonvertidos para 0 (PRETO)
+	# pixels de intensidade de cor acima de 127 saonvertidos para 255 (BRANCO)
+	# A atrubiao do THRESH_OTSU incrementa uma anateligente dos nivels de truncagem
 	ret, thresh = cv2.threshold(im, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+	# Visualizaao da imagem
+	#cv2.imshow('image',thresh)
+	#cv2.waitKey(0)
+	#cv2.destroyAllWindows()
 
 	# reconvertendo o retorno do threshold em um objeto do tipo PIL.Image
 	binimagem = Image.fromarray(thresh)
@@ -48,71 +97,41 @@ def readImage():
 	# imprao do resultado
 	return phrase
 
+# Now keep talking with the client
+while 1:
+    # Wait to accept a connection - blocking call
+	conn, addr = s.accept()
+	print ('Connected with ' + addr[0] + ':' + str(addr[1]))
+	final = ""
+	size = ""
+	data = conn.recv(1024)
+	size += data.decode('utf-8')
+	data = conn.recv(1024)
+	size += data.decode('utf-8')
+	size = int(size)
+	print(size)
+	# Infinite loop so that function do not terminate
+	while True:
+		#print("Looking for more")
+		dataImg = conn.recv(1024)
+		#print(data)
+		final += dataImg.decode('utf-8')
+		print(dataImg.decode('utf-8'))
+		size -= 1024
+		if size <= 0 :
+			print("Breaking")
+			break
 
-def main():
+	print("Received all")
+	#print (final)
+	#print(size)
+	decodeImage(final)
+	reply = readImage()
+	print (reply)
+	#conn.sendall(str(len(reply.encode())).encode())
+	conn.sendall(reply.encode())
 
-	# "192.168.42.85"
-	HOST = "192.168.42.193"	# Symbolic name meaning all available interfaces
-	PORT = 8888	# Arbitrary non-privileged port
-	HOST = socket.gethostbyname(HOST)
-	print(str(HOST))
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	print ('Socket created')
+	# End of Loop
 
-	#Bind socket to local host and port
-	try:
-		s.bind((HOST, PORT))
-	except socket.error as msg:
-		print ('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-		sys.exit()
-
-	print ('Socket bind complete')
-
-	# Start listening on socket
-	s.listen(10)
-	print ('Socket now listening')
-
-	# Now keep talking with the client
-	while 1:
-	    # Wait to accept a connection - blocking call
-		conn, addr = s.accept()
-		print ('Connected with ' + addr[0] + ':' + str(addr[1]))
-		final = ""
-		size = 0
-		# data = conn.recv(4)
-		# print("Received size: " + str(size.from_bytes(data,'big')))
-		# size = size.from_bytes(data,'big')
-
-		# Infinite loop so that function do not terminate
-		while True:
-			# print("Looking for more")
-			# Receiving from client
-			data = conn.recv(1024)
-			final += data.decode("utf-8")
-			# size -= 1024
-			#  print(size)
-			# if size <= 0:
-			if not data:
-				print("Breaking")
-				break
-
-		# print("Received all")
-		decodeImage(final)
-		reply = readImage()
-		# 	#reply = readImage()
-		print(reply)
-		if reply == "":
-			reply= "Não consegui ler"
-		myobj = gTTS(text=reply, lang='pt', slow=False)
-		myobj.save("speech.mp3")
-		os.system("mpg321 speech.mp3")
-
-
-		# conn.sendall(reply.encode())
-		# # End of Loop
-
-	conn.close()
-	s.close()
-
-if __name__ == "__main__":
-	main()
+conn.close()
+s.close()
